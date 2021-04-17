db786057c9e6b733f9044d96ddd9f9aaffa3fd3f
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-04-17 17:59:29 -0300

    feito google forms pratica

---
+++ tests/google.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "@tags": ["google"],
+  "Google advenceds search: Elon Musk"(browser) {
+    const mainQuery = "Elon Musk";
+
+    const mainQueryInputSelector = 'input[name="as_q"]';
+    const languageDropdownOpenerSelector = "#lr_button";
+    const languageDropdownValueSelector = 'li[value="lang_pt"]';
+    const lasUpadtedDropdownOpenerSelector = "#as_qdr_button";
+    const lasUpadtedDropdownValueSelector = '.goog-menuitem[value="m"]';
+    const submitButtonSelector = '.jfk-button[type="submit"]';
+
+    const resultPageQuerySelector = `#searchform input[name="q"][value="${mainQuery}"]`;
+    const resultPageQLanguageSelector = ".KTBKoe";
+
+    browser
+      .url("https://www.google.com/advanced_search")
+      .setValue(mainQueryInputSelector, mainQuery)
+      .click(languageDropdownOpenerSelector)
+      .click(languageDropdownValueSelector)
+      .click(lasUpadtedDropdownOpenerSelector)
+      .click(lasUpadtedDropdownValueSelector)
+      .click(submitButtonSelector)
+      .assert.urlContains("as_q=Elon+Musk", "Params: Query is Elon Musk")
+      .assert.urlContains("lr=lang_pt", "Params: Language is Portuguese")
+      .assert.urlContains("as_qdr=m", "Params: Time period is last month");
+
+    browser.assert.visible(
+      resultPageQuerySelector,
+      `UI: ${mainQuery} is set in the query`
+    );
+
+    browser.assert.containsText(
+      ".KTBKoe",
+      "Pesquisar páginas em Português",
+      "UI: Language set to portuguese"
+    );
+    
+    browser.assert.containsText(
+      resultPageQLanguageSelector,
+      "No último mês",
+      "UI: Last update set o past month"
+    );
+    browser.saveScreenshot("tests_output/google.png");
+  },
+};
